b3b634526386b0c91ebddea51095956a0727330e
Candice <xgao58@fordham.edu>
2020-03-25 16:50:58 -0700

    Define count function

--- wordocunt/views.py
+++ wordocunt/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def homepage(request):
     return render(request, 'home.html')
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+
+    worddic = {}
+    for word in wordlist:
+        if word not in worddic.keys():
+            worddic[word] = 1
+        else:
+            worddic[word] += 1
+    sortedwords = sorted(worddic.items(), key = operator.itemgetter(1), reverse = True)
+
+
+
+    return render(request, 'count.html', {'completetext':fulltext, 'count': len(wordlist), 'sort_w': sortedwords})
+    # items() method is used to return the list with all the dictionalry keys with values
+    '''
+    https://www.geeksforgeeks.org/python-dictionary-items-method/
+    '''
